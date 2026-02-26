8f6682a9ba6e13fd354d38f68a1615d12eb4deeb
JuliaAntequera <jantequeradu.inf@upsa.es>
2026-02-26 13:47:55 +0100

    commit inicial

---
+++ app_gestion_taller/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import lista_clientes, detalle_cliente
+urlpatterns = [
+    path('clientes/', lista_clientes, name='lista_clientes'),
+    path('clientes/<int:cliente_id>/', detalle_cliente, name='detalle_cliente'),
+]
